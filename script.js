c3233bbda3516094fedda3b6185ce62527b8b866
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2024-02-02 09:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let num1;
+num1 = prompt();
+num1 = parseFloat(num1);
+let num2 = prompt();
+num2 = parseFloat(num2);
+
+let result = (num1*2)+num2;
+alert(result);
